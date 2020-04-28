758b779d137672cf1d58db011628715eea933068
luizAzevedo <lfaazevedo@hotmail.com>
2020-04-28 19:22:07 -0300

    masterclass upload frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
@@ -27,6 +28,7 @@ mongoose.connect(
 
 //.catch((err) => console.log(err));
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
@@ -36,4 +38,4 @@ app.use(
 );
 app.use(require('./routes'));
 
-app.listen(3000);
+app.listen(3333);
---
+++ frontend/README.md
@@ -0,0 +1,21 @@
+##### Instalações
+
+create-react-app frontend
+
+yarn add styled-components
+
+yarn add -D typescript-styled-plugin
+
+yarn add reac-dropzone
+
+yarn add dotenv
+
+yarn add react-circular-progressbar
+
+yarn add react-icons
+
+yarn add lodash 
+
+yarn add filesize
+
+yarn add axios  
